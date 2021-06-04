1b9c22d25731a35b58ca49fe0e5d14d95139cdd3
nazmuluzzal <uzzalnazmul@gmail.com>
2021-06-04 23:40:42 +0600

    [fixed] some errors

--- routes/posts.js
+++ routes/posts.js
@@ -1,133 +1,146 @@
-const express = require('express')
-const router = express.Router()
-const mongoose = require('mongoose')
-const protected  = require('../access/access')
-const Post =  mongoose.model("Post")
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const protected = require("../access/access");
+const Post = mongoose.model("Post");
 
-
-router.get('/allpost',protected,(req,res)=>{
-    Post.find()
-    .populate("postedBy","_id name")
-    .populate("comments.postedBy","_id name")
-    .sort('-createdAt')
-    .then((posts)=>{
-        res.json({posts})
-    }).catch(err=>{
-        console.log(err)
+router.get("/allpost", protected, (req, res) => {
+  Post.find()
+    .populate("postedBy", "_id name")
+    .populate("comments.postedBy", "_id name")
+    .sort("-createdAt")
+    .then((posts) => {
+      res.json({ posts });
     })
-    
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-router.get('/getsubpost',protected,(req,res)=>{
-
-    Post.find({postedBy:{$in:req.user.following}})
-    .populate("postedBy","_id name")
-    .populate("comments.postedBy","_id name")
-    .sort('-createdAt')
-    .then(posts=>{
-        res.json({posts})
+router.get("/getsubpost", protected, (req, res) => {
+  Post.find({ postedBy: { $in: req.user.following } })
+    .populate("postedBy", "_id name")
+    .populate("comments.postedBy", "_id name")
+    .sort("-createdAt")
+    .then((posts) => {
+      res.json({ posts });
     })
-    .catch(err=>{
-        console.log(err)
-    })
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-router.post('/createpost',protected,(req,res)=>{
-    const {title,body,pic} = req.body 
-    if(!title || !body || !pic){
-      return  res.status(422).json({error:"Plase add all the fields"})
+router.post("/createpost", protected, (req, res) => {
+  const { title, body, pic } = req.body;
+  if (!title || !body || !pic) {
+    return res.status(422).json({ error: "Plase add all the fields" });
+  }
+  req.user.password = undefined;
+  const post = new Post({
+    title,
+    body,
+    photo: pic,
+    postedBy: req.user,
+  });
+  post
+    .save()
+    .then((result) => {
+      res.json({ post: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get("/mypost", protected, (req, res) => {
+  Post.find({ postedBy: req.user._id })
+    .populate("PostedBy", "_id name")
+    .then((mypost) => {
+      res.json({ mypost });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.put("/like", protected, (req, res) => {
+  Post.findByIdAndUpdate(
+    req.body.postId,
+    {
+      $push: { likes: req.user._id },
+    },
+    {
+      new: true,
     }
-    req.user.password = undefined
-    const post = new Post({
-        title,
-        body,
-        photo:pic,
-        postedBy:req.user
-    })
-    post.save().then(result=>{
-        res.json({post:result})
-    })
-    .catch(err=>{
-        console.log(err)
-    })
-})
-
-router.get('/mypost',protected,(req,res)=>{
-    Post.find({postedBy:req.user._id})
-    .populate("PostedBy","_id name")
-    .then(mypost=>{
-        res.json({mypost})
-    })
-    .catch(err=>{
-        console.log(err)
-    })
-})
-
-router.put('/like',protected,(req,res)=>{
-    Post.findByIdAndUpdate(req.body.postId,{
-        $push:{likes:req.user._id}
-    },{
-        new:true
-    }).exec((err,result)=>{
-        if(err){
-            return res.status(422).json({error:err})
-        }else{
-            res.json(result)
-        }
-    })
-})
-router.put('/unlike',protected,(req,res)=>{
-    Post.findByIdAndUpdate(req.body.postId,{
-        $pull:{likes:req.user._id}
-    },{
-        new:true
-    }).exec((err,result)=>{
-        if(err){
-            return res.status(422).json({error:err})
-        }else{
-            res.json(result)
-        }
-    })
-})
-
-
-router.put('/comment',protected,(req,res)=>{
-    const comment = {
-        text:req.body.text,
-        postedBy:req.user._id
+  ).exec((err, result) => {
+    if (err) {
+      return res.status(422).json({ error: err });
+    } else {
+      res.json(result);
     }
-    Post.findByIdAndUpdate(req.body.postId,{
-        $push:{comments:comment}
-    },{
-        new:true
-    })
-    .populate("comments.postedBy","_id name")
-    .populate("postedBy","_id name")
-    .exec((err,result)=>{
-        if(err){
-            return res.status(422).json({error:err})
-        }else{
-            res.json(result)
-        }
-    })
-})
+  });
+});
+router.put("/unlike", protected, (req, res) => {
+  Post.findByIdAndUpdate(
+    req.body.postId,
+    {
+      $pull: { likes: req.user._id },
+    },
+    {
+      new: true,
+    }
+  ).exec((err, result) => {
+    if (err) {
+      return res.status(422).json({ error: err });
+    } else {
+      res.json(result);
+    }
+  });
+});
 
-router.delete('/deletepost/:postId',protected,(req,res)=>{
-    Post.findOne({_id:req.params.postId})
-    .populate("postedBy","_id")
-    .exec((err,post)=>{
-        if(err || !post){
-            return res.status(422).json({error:err})
-        }
-        if(post.postedBy._id.toString() === req.user._id.toString()){
-              post.remove()
-              .then(result=>{
-                  res.json(result)
-              }).catch(err=>{
-                  console.log(err)
-              })
-        }
-    })
-})
+router.put("/comment", protected, (req, res) => {
+  const comment = {
+    text: req.body.text,
+    postedBy: req.user._id,
+  };
+  Post.findByIdAndUpdate(
+    req.body.postId,
+    {
+      $push: { comments: comment },
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("comments.postedBy", "_id name")
+    .populate("postedBy", "_id name")
+    .exec((err, result) => {
+      if (err) {
+        return res.status(422).json({ error: err });
+      } else {
+        res.json(result);
+      }
+    });
+});
 
-module.exports = router
+router.delete("/deletepost/:postId", protected, (req, res) => {
+  Post.findOne({ _id: req.params.postId })
+    .populate("postedBy", "_id")
+    .exec((err, post) => {
+      if (err || !post) {
+        return res.status(422).json({ error: err });
+      }
+      if (post.postedBy._id.toString() === req.user._id.toString()) {
+        post
+          .remove()
+          .then((result) => {
+            res.json(result);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    });
+});
+
+module.exports = router;
